3c95c8c84a652e77976d820239961d34b7df1da4
Rinor Nishevci <rinornishevci@gmail.com>
2023-07-19 20:22:45 +0200

    cloudfront updated.

--- terrafrom/cloudfront.tf
+++ terrafrom/cloudfront.tf
@@ -1,49 +1,36 @@
+resource "aws_cloudfront_origin_access_control" "web_client" {
+  name                              = "rinor_web_client_oac"
+  description                       = "Grant access to s3 media bucket"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
 module "web_client_cdn" {
   source  = "terraform-aws-modules/cloudfront/aws"
   version = "3.2.1"
   #aliases = [""]
-
   comment         = "CDN for web-client"
   enabled         = true
   is_ipv6_enabled = true
   price_class     = "PriceClass_All"
-
-  create_origin_access_identity = true
-  origin_access_identities = {
-    s3_web_client = "Web Client Bucket"
-  }
-
-
-  origin = {
+default_root_object = "index.html"
+  create_origin_access_identity = false
+ origin = {
     web_client = {
-      domain_name = "tali.assignment-4.appstellar.training"
-      custom_origin_config = {
-        http_port              = 80
-        https_port             = 443
-        origin_protocol_policy = "match-viewer"
-        origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
-      }
-    }
-
-    s3_web_client = {
-      domain_name = module.web_client.s3_bucket_bucket_domain_name
-      s3_origin_config = {
-        origin_access_identity = "s3_web_client"
-      }
+      domain_name              = module.web_client.s3_bucket_bucket_domain_name
+      origin_access_control_id = aws_cloudfront_origin_access_control.web_client.id
     }
   }
-
-  default_cache_behavior = {
-    target_origin_id           = "web_client"
-    viewer_protocol_policy     = "allow-all"
-
-    allowed_methods = ["GET", "HEAD", "OPTIONS"]
-    cached_methods  = ["GET", "HEAD"]
-    compress        = true
-    query_string    = true
+default_cache_behavior = {
+    target_origin_id       = "web_client"
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
+    cached_methods         = ["GET", "HEAD"]
+    compress               = true
+    min_ttl     = 1
+    default_ttl = 86400
+    max_ttl     = 31536000
   }
-
-
   #   viewer_certificate = {
   #     acm_certificate_arn = "arn:aws:acm:us-east-1:135367859851:certificate/1032b155-22da-4ae0-9f69-e206f825458b"
   #     ssl_support_method  = "sni-only"
